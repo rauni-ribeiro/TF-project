d0a0eaee9a2583ede88374609fd0524561faaad3
Rauni Ribeiro <raunirr98@gmail.com>
2024-03-05 16:58:25 -0300

    Adding a default name to our EC2 instance for future developments

--- main.tf
+++ main.tf
@@ -8,6 +8,9 @@ resource "aws_instance" "web-server-sample" {
   ami           = var.ami
   instance_type = var.instance_type
   key_name      = var.key_name
+  tags = {
+    Name = "web-server-sample"
+  }
 }
 
 
