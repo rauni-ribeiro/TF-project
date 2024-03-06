13dd98cd768d87a4e7ef546a31cf4cb82f9ba7e3
Rauni Ribeiro <raunirr98@gmail.com>
2024-03-05 21:16:33 -0300

    Adding sudo to webapp-script.sh

--- webapp-script.sh
+++ webapp-script.sh
@@ -8,8 +8,8 @@ yum update -y
 yum install -y httpd
 
 # Start and enable Apache server
-systemctl start httpd
-systemctl enable httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
 
 # Create a simple HTML page
-echo "<h1>Welcome to my website</h1>" > /var/www/html/index.html
+echo "<h1>Hello World!</h1>" > /var/www/html/index.html
