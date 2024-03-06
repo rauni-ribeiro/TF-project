150c19338fb90cf012f45ca495a26714e4d5fcdf
Rauni Ribeiro <raunirr98@gmail.com>
2024-03-06 14:20:19 -0300

    Modifying AWS credentials.sh & buildspec.yml

--- aws-credentials.sh
+++ aws-credentials.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-# Retrieve the secret values from AWS Secrets Manager
-secret_response=$(aws secretsmanager get-secret-value --secret-id MyAccessSecret)
-
-# Extract access key and secret key from the response
-access_key=$(jq -r '.SecretString | fromjson | .access_key' <<< "$secret_response")
-secret_key=$(jq -r '.SecretString | fromjson | .secret_key' <<< "$secret_response")
-
-# Configure AWS CLI with the retrieved credentials
-aws configure set aws_access_key_id $access_key
-aws configure set aws_secret_access_key $secret_key
+export AWS_ACCESS_KEY_ID="YOUR_ACCESS_KEY_ID"
+export AWS_SECRET_ACCESS_KEY="YOUR_SECRET_ACCESS_KEY"
+export AWS_DEFAULT_REGION="YOUR_DEFAULT_REGION"
 
 # Use AWS CLI for authentication and other operations
 aws ec2 describe-instances
