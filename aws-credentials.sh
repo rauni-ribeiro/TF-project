627c08630ae8fa99efe1b3de48fc3537b8b365a8
Rauni Ribeiro <raunirr98@gmail.com>
2024-03-06 02:19:40 -0300

    Updated aws-credentiails.sh

--- aws-credentials.sh
+++ aws-credentials.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-#This will assign my environment variables to $my_access_key & $my_secret_key,
-#meaning, I don`t need to use "aws configure" to sign in, anymore.
-aws configure set aws_access_key_id $my_access_key
-aws configure set aws_secret_access_key $my_secret_key
+# Retrieve the secret values from AWS Secrets Manager
+secret_response=$(aws secretsmanager get-secret-value --secret-id MyAccessSecret)
+
+# Extract access key and secret key from the response
+access_key=$(jq -r '.SecretString | fromjson | .access_key' <<< "$secret_response")
+secret_key=$(jq -r '.SecretString | fromjson | .secret_key' <<< "$secret_response")
+
+# Configure AWS CLI with the retrieved credentials
+aws configure set aws_access_key_id $access_key
+aws configure set aws_secret_access_key $secret_key
+
+# Use AWS CLI for authentication and other operations
+aws ec2 describe-instances
