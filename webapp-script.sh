80f1a4566001e6272d9804b3fe7d1d1ce5ac1086
Rauni Ribeiro <raunirr98@gmail.com>
2024-03-05 21:07:15 -0300

    Update on webapp-script.sh  |  redid the entire script for better reading

--- webapp-script.sh
+++ webapp-script.sh
@@ -1,31 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-#exit on failure mode
+# Exit on failure mode
 set -eu
 
-# Get private IP address of the newly deployed EC2 instance
-private_ip=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=webserver" --query "Reservations[*].Instances[*].PrivateIpAddress" --output text)
+# Install Apache HTTP server
+yum update -y
+yum install -y httpd
 
-# Connect to EC2 instance using private IP address
-ssh -i tfproject-key.pem ec2-user@$private_ip
+# Start and enable Apache server
+systemctl start httpd
+systemctl enable httpd
 
-
-<<-EOF
-    #!/bin/bash
-    yum update -y
-    yum install -y httpd
-    systemctl start httpd
-    systemctl enable httpd
-    echo "<h1>Welcome to my website</h1>" > /var/www/html/index.html
-EOF
-
-
-
-#GUIDE
-
-# yum install -y httpd = installs apache HTTP server
-#systemctl start httpd = starts apache HTTP server
-#systemctl enable httpd = enables apache HTTP server
-#finally, writes the HTML content to /var/www/html/index.html
-#.
-
+# Create a simple HTML page
+echo "<h1>Welcome to my website</h1>" > /var/www/html/index.html
