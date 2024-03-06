1b8a7d55c15d5025121b8203dc8d0dc8e0a836ff
Rauni Ribeiro <raunirr98@gmail.com>
2024-03-06 17:39:22 -0300

    Modifying Buildspec.yml (Build) & webapp-script.sh

--- webapp-script.sh
+++ webapp-script.sh
@@ -8,9 +8,9 @@ yum update -y
 yum install -y httpd
 
 # Start and enable Apache server
-sleep 10
-sudo systemctl start httpd
-sudo systemctl enable httpd
+sleep 4
+systemctl start httpd
+systemctl enable httpd
 
 # Create a simple HTML page
 echo "<h1>Hello World!</h1>" > /var/www/html/index.html
