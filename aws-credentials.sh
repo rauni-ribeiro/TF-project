a8f4531e951f57d4f3880868ff354566696a8be0
Rauni Ribeiro <raunirr98@gmail.com>
2024-03-06 14:53:30 -0300

    Modifying AWS-credentials.sh var names.

--- aws-credentials.sh
+++ aws-credentials.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
-export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
-export AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
+export AWS_ACCESS_KEY_ID=$access_key
+export AWS_SECRET_ACCESS_KEY=$secret_key
+export AWS_DEFAULT_REGION=$region_key
 
 # Use AWS CLI for authentication and other operations
 aws ec2 describe-instances
