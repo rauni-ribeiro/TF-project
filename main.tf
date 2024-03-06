dbd3cc97250df2906d500f905b8ff574bcd8de77
Rauni Ribeiro <raunirr98@gmail.com>
2024-03-05 22:21:47 -0300

    Adding a security group to main.tf

--- main.tf
+++ main.tf
@@ -21,3 +21,34 @@ terraform {
     profile = "default"
   }
 }
+
+#creating security group for our project
+resource "aws_security_group" "SG" {
+  name        = "TFproject-SG"
+  description = "Allow HTTP and SSH traffic from any source"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "example-security-group"
+  }
+}
