902d8b9a2e964d9577b1ef4095baca4c94cb42c6
Rauni Ribeiro <raunirr98@gmail.com>
2024-03-08 02:15:25 -0300

    Modify / policy attachment fix on line 75

--- main.tf
+++ main.tf
@@ -73,8 +73,7 @@ resource "aws_iam_role" "ec2_role" {
 }
 
 #creating the policy attachment for our IAM role (ec2_role)
-resource "aws_iam_policy_attachment" "ec2_policy_attachment" {
-  name = "TFproject_iam_policy_attachment"
-  roles = [aws_iam_role.ec2_role.name] #Referencing the IAM role we just created
+resource "aws_iam_role_policy_attachment" "ec2_policy_attachment" {
+  role = aws_iam_role.ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildReadOnlyAccess"
 }
