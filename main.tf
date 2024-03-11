35961611a037ab768874e3a17c6b1854dcb47cf7
Rauni Ribeiro <raunirr98@gmail.com>
2024-03-11 16:04:19 -0300

    Adding Route Table + associating it to our public subnet.

--- main.tf
+++ main.tf
@@ -112,3 +112,23 @@ resource "aws_subnet" "tf_subnet" {
     Name = "TFproject-Subnet"
   }
 }
+
+#Creating a Route Table
+resource "aws_route_table" "tf_route_table" {
+  vpc_id = aws_vpc.tf_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.tf_igw.id
+  }
+
+  tags = {
+    Name = "TFproject-RouteTable"
+  }
+}
+
+#Associating the Route Table with the Subnet
+resource "aws_route_table_association" "tf_subnet_association" {
+  subnet_id      = aws_subnet.tf_subnet.id
+  route_table_id = aws_route_table.tf_route_table.id
+}
