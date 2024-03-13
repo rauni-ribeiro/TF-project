40dc8a011447ada83b395e7bf2136043ad9cbc75
Rauni Ribeiro <raunirr98@gmail.com>
2024-03-13 17:07:19 -0300

    Removing changes from webapp-scripts.sh / adding sleep time

--- shell-scripts/webapp-script.sh
+++ shell-scripts/webapp-script.sh
@@ -11,17 +11,20 @@ sudo yum update -y
 sudo yum install -y httpd
 
 # Start and enable Apache server
-sleep 10  #waiting for the Apache server installation to complete
-sudo service httpd start
-sudo chkconfig httpd on
+sleep 30 #waiting for the Apache server installation to complete
+sudo systemctl start httpd
+sleep 5
+sudo systemctl enable httpd
 
 # checking for current status of Apache server
-sudo service httpd status
+sudo systemctl status httpd
+sleep 5
 
 # Copying the index.html file from our S3 bucket to /var/www/html/ (this is our local machine's directory)
 sudo aws s3 cp s3://tfproject-html/index.html /var/www/html/ --metadata-directive REPLACE --acl public-read
+sleep 5
 
 # Restarting Apache server in order to fetch the content of the index.html file
-sudo service httpd restart
+sudo systemctl restart httpd
 
 # NOTE: Now, on your browser, copy your public ipv4 address and add it with port :80 (at the ending) to check whether the server is indeed running. Ex: 10.123.533.4:80  / <IPv4address>:80
