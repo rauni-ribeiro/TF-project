745a50cf8294e760569ada0fac4940cad76fec9c
Rauni Ribeiro <raunirr98@gmail.com>
2024-03-13 16:52:18 -0300

    Modifying webapp-scripts.sh  / replacing systemd command with other service commands

--- shell-scripts/webapp-script.sh
+++ shell-scripts/webapp-script.sh
@@ -12,16 +12,16 @@ sudo yum install -y httpd
 
 # Start and enable Apache server
 sleep 10  #waiting for the Apache server installation to complete
-sudo systemctl start httpd
-sudo systemctl enable httpd
+sudo service httpd start
+sudo chkconfig httpd on
 
 # checking for current status of Apache server
-sudo systemctl status httpd
+sudo service httpd status
 
 # Copying the index.html file from our S3 bucket to /var/www/html/ (this is our local machine's directory)
 sudo aws s3 cp s3://tfproject-html/index.html /var/www/html/ --metadata-directive REPLACE --acl public-read
 
 # Restarting Apache server in order to fetch the content of the index.html file
-sudo systemctl restart httpd
+sudo service httpd restart
 
 # NOTE: Now, on your browser, copy your public ipv4 address and add it with port :80 (at the ending) to check whether the server is indeed running. Ex: 10.123.533.4:80  / <IPv4address>:80
