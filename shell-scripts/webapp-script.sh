14b5ab68112bde52e1c540241acba3510d855ab5
Rauni Ribeiro <raunirr98@gmail.com>
2024-03-17 19:05:46 -0300

    Modifying webapp-script.sh and deleting start-apache-server.sh

--- shell-scripts/webapp-script.sh
+++ shell-scripts/webapp-script.sh
@@ -6,7 +6,16 @@ pwd
 # exit on failure mode
 set -eu
 
-# Install Apache HTTP server
+#Create an apache-script.sh file with the following contents:
+cat << 'EOF' > apache-script.sh
+
 sudo yum update -y
-sleep 4
 sudo yum install -y httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo aws s3 cp s3://tfproject-html/index.html /var/www/html/ --metadata-directive REPLACE --acl public-read
+sudo systemctl restart httpd
+EOF
+
+sudo chmod +x apache-script.sh
+sudo ./apache-script.sh
