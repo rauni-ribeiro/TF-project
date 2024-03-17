ce5cf10e0b2a8b3090bca2955bd00da6c3badb21
Rauni Ribeiro <raunirr98@gmail.com>
2024-03-17 18:33:19 -0300

    Modifying start-apache-server to systemctl

--- shell-scripts/start-apache-server.sh
+++ shell-scripts/start-apache-server.sh
@@ -1,14 +1,14 @@
 # Start Apache server
-sudo service httpd start
+sudo systemctl httpd start
 
 # Enable Apache server to start on boot
-sudo chkconfig httpd on
+sudo systemctl httpd on
 
 # Checking the status of Apache server
-sudo service httpd status
+sudo systemctl httpd status
 
 # Copying the index.html file from S3 bucket to /var/www/html/
 sudo aws s3 cp s3://tfproject-html/index.html /var/www/html/ --metadata-directive REPLACE --acl public-read
 
 # Restart Apache server to fetch the content of index.html
-sudo service httpd restart
+sudo systemctl start httpd
