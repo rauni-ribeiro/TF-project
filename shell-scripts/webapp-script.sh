b8060bc341b30bbc183ee3a8a1b077aa4b1be2c3
Rauni Ribeiro <raunirr98@gmail.com>
2024-03-17 19:17:30 -0300

    Modifying - using bash -c to execute the .sh commands

--- shell-scripts/webapp-script.sh
+++ shell-scripts/webapp-script.sh
@@ -6,17 +6,14 @@ pwd
 # exit on failure mode
 set -eu
 
-#Create an apache-script.sh file with the following contents:
-cat << 'EOF' > apache-script.sh
-
+# Execute the commands directly instead of writing to a file
+bash -c '
 sudo yum update -y
 sudo yum install -y httpd
 sudo systemctl start httpd
 sudo systemctl enable httpd
 sudo aws s3 cp s3://tfproject-html/index.html /var/www/html/ --metadata-directive REPLACE --acl public-read
 sudo systemctl restart httpd
-EOF
+'
 
 sleep 10
-#sudo chmod +x apache-script.sh
-#sudo ./apache-script.sh
