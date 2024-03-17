c4ee158daf1f2dd56dd5394f4d3e2721083c7f38
Rauni Ribeiro <raunirr98@gmail.com>
2024-03-17 18:23:38 -0300

    Replacing webscript.sh with sysvinit commands

--- shell-scripts/webapp-script.sh
+++ shell-scripts/webapp-script.sh
@@ -8,19 +8,20 @@ set -eu
 
 # Install Apache HTTP server
 sudo yum update -y
+sleep 4
 sudo yum install -y httpd
 
-# Start and enable Apache server
-sudo systemctl start httpd
-sudo systemctl enable httpd
+# Start Apache server
+sudo service httpd start
 
-# checking for current status of Apache server
-sudo systemctl status httpd
+# Enable Apache server to start on boot
+sudo chkconfig httpd on
 
-# Copying the index.html file from our S3 bucket to /var/www/html/ (this is our local machine's directory)
+# Checking the status of Apache server
+sudo service httpd status
+
+# Copying the index.html file from S3 bucket to /var/www/html/
 sudo aws s3 cp s3://tfproject-html/index.html /var/www/html/ --metadata-directive REPLACE --acl public-read
 
-# Restarting Apache server in order to fetch the content of the index.html file
-sudo systemctl restart httpd
-
-# NOTE: Now, on your browser, copy your public ipv4 address and add it with port :80 (at the ending) to check whether the server is indeed running. Ex: 10.123.533.4:80  / <IPv4address>:80
+# Restart Apache server to fetch the content of index.html
+sudo service httpd restart
