20b781e7b8f314dfe52b3bdd5249d90dfe11b352
Rauni Ribeiro <raunirr98@gmail.com>
2024-03-17 19:37:27 -0300

    Modifying systemd check before execution

--- shell-scripts/webapp-script.sh
+++ shell-scripts/webapp-script.sh
@@ -6,14 +6,26 @@ pwd
 # exit on failure mode
 set -eu
 
-# Execute the commands directly instead of writing to a file
-bash -c '
-sudo yum update -y
-sudo yum install -y httpd
-sudo systemctl start httpd
-sudo systemctl enable httpd
-sudo aws s3 cp s3://tfproject-html/index.html /var/www/html/ --metadata-directive REPLACE --acl public-read
-sudo systemctl restart httpd
-'
+# Check if systemd is available
+if [ -x /bin/systemctl ]; then
+    # Install Apache HTTP server
+    sudo yum update -y
+    sudo yum install -y httpd
 
-sleep 10
+    # Start and enable Apache server
+    sudo systemctl start httpd
+    sudo systemctl enable httpd
+
+    # checking for current status of Apache server
+    sudo systemctl status httpd
+
+    # Copying the index.html file from our S3 bucket to /var/www/html/ (this is our local machine's directory)
+    sudo aws s3 cp s3://tfproject-html/index.html /var/www/html/ --metadata-directive REPLACE --acl public-read
+
+    # Restarting Apache server in order to fetch the content of the index.html file
+    sudo systemctl restart httpd
+
+    # NOTE: Now, on your browser, copy your public ipv4 address and add it with port :80 (at the ending) to check whether the server is indeed running. Ex: 10.123.533.4:80  / <IPv4address>:80
+else
+    echo "System is not using systemd as init system. Unable to perform systemd-specific operations."
+fi
