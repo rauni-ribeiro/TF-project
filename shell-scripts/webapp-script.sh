06da4fea2af15fe49ac7c008023f79cfdeb299fb
Rauni Ribeiro <raunirr98@gmail.com>
2024-03-17 18:27:58 -0300

    Adding a script to start the apache server

--- shell-scripts/webapp-script.sh
+++ shell-scripts/webapp-script.sh
@@ -10,18 +10,3 @@ set -eu
 sudo yum update -y
 sleep 4
 sudo yum install -y httpd
-
-# Start Apache server
-sudo service httpd start
-
-# Enable Apache server to start on boot
-sudo chkconfig httpd on
-
-# Checking the status of Apache server
-sudo service httpd status
-
-# Copying the index.html file from S3 bucket to /var/www/html/
-sudo aws s3 cp s3://tfproject-html/index.html /var/www/html/ --metadata-directive REPLACE --acl public-read
-
-# Restart Apache server to fetch the content of index.html
-sudo service httpd restart
