3439060fbe28ab5f2609a3080806451a380af1f6
Rauni Ribeiro <raunirr98@gmail.com>
2024-03-18 00:05:04 -0300

    Modifying systemd=true

--- shell-scripts/webapp-script.sh
+++ shell-scripts/webapp-script.sh
@@ -6,32 +6,27 @@ pwd
 # exit on failure mode
 set -eu
 
-# Check if systemd is available
-if [ -x /bin/systemctl ]; then
-    # Add systemd configuration to wsl.conf
-    sudo nano /etc/wsl.conf <<EOF
+# Add systemd configuration to wsl.conf
+sudo nano /etc/wsl.conf <<EOF
 [boot]
 systemd=true
 EOF
 
-    # Install Apache HTTP server
-    sudo yum update -y
-    sudo yum install -y httpd
+# Install Apache HTTP server
+sudo yum update -y
+sudo yum install -y httpd
 
-    # Start and enable Apache server
-    sudo systemctl start httpd
-    sudo systemctl enable httpd
+# Start and enable Apache server
+sudo systemctl start httpd
+sudo systemctl enable httpd
 
-    # checking for current status of Apache server
-    sudo systemctl status httpd
+# checking for current status of Apache server
+sudo systemctl status httpd
 
-    # Copying the index.html file from our S3 bucket to /var/www/html/ (this is our local machine's directory)
-    sudo aws s3 cp s3://tfproject-html/index.html /var/www/html/ --metadata-directive REPLACE --acl public-read
+# Copying the index.html file from our S3 bucket to /var/www/html/ (this is our local machine's directory)
+sudo aws s3 cp s3://tfproject-html/index.html /var/www/html/ --metadata-directive REPLACE --acl public-read
 
-    # Restarting Apache server in order to fetch the content of the index.html file
-    sudo systemctl restart httpd
+# Restarting Apache server in order to fetch the content of the index.html file
+sudo systemctl restart httpd
 
-    # NOTE: Now, on your browser, copy your public ipv4 address and add it with port :80 (at the ending) to check whether the server is indeed running. Ex: 10.123.533.4:80  / <IPv4address>:80
-else
-    echo "System is not using systemd as init system. Unable to perform systemd-specific operations."
-fi
+# NOTE: Now, on your browser, copy your public ipv4 address and add it with port :80 (at the ending) to check whether the server is indeed running. Ex: 10.123.533.4:80  / <IPv4address>:80
